8cc81cfbbf53f6dd0bb916c3abe98f44a6131044
ipersids <ipersids@student.hive.fi>
2025-02-26 16:29:57 +0200

    Add uncompleted changes

--- philo-threads/src/simulation_actions.c
+++ philo-threads/src/simulation_actions.c
@@ -6,123 +6,45 @@
 /*   By: ipersids <ipersids@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 13:53:40 by ipersids          #+#    #+#             */
-/*   Updated: 2025/02/25 15:35:29 by ipersids         ###   ########.fr       */
+/*   Updated: 2025/02/26 16:22:40 by ipersids         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-/* --------------------- Private function prototypes ----------------------- */
-
-/**
- * @brief Attempts to lock a mutex with a delay.
- * 
- * @param i Index of the mutex to lock.
- * @param delay Delay between attempts to lock the mutex.
- * @param philo Pointer to the philosopher structure.
- * @param whoami Pointer to the whoami structure.
- * @return EXIT_SUCCESS if the mutex is locked, EXIT_FAILURE otherwise.
- */
-static int	trylock(size_t i, int delay, t_philo *philo, t_whoami *whoami);
-
 /* --------------------------- Public Functions ---------------------------- */
 
-int	philo_routine_go_think(t_philo *philo, t_whoami *whoami)
+void	philo_routine_go_think(t_philo *philo, t_whoami *whoami)
 {
-	if (philo_is_alive_check(whoami, philo))
-	{
-		philo_print_message(MSG_THINK, philo, whoami);
-		return (EXIT_SUCCESS);
-	}
-	return (EXIT_FAILURE);
+	philo_print_message(MSG_THINK, philo, whoami);
 }
 
-int	philo_routine_go_take_fork(t_philo *philo, t_whoami *whoami)
+void	philo_routine_go_take_fork(t_philo *philo, t_whoami *whoami)
 {
-	if (EXIT_FAILURE == trylock(whoami->left_i, TIME_DELAY, philo, whoami))
-		return (EXIT_FAILURE);
-	if (EXIT_FAILURE == trylock(whoami->right_i, TIME_DELAY, philo, whoami))
-	{
-		pthread_mutex_unlock(&philo->thread_lock[whoami->left_i]);
-		return (EXIT_FAILURE);
-	}
-	if (philo_is_alive_check(whoami, philo))
-	{
-		philo_print_message(MSG_FORK, philo, whoami);
-		return (EXIT_SUCCESS);
-	}
-	pthread_mutex_lock(&philo->try_lock);
-	philo->thread_bool[whoami->left_i] = IS_FREE;
-	philo->thread_bool[whoami->right_i] = IS_FREE;
-	pthread_mutex_unlock(&philo->try_lock);
 	pthread_mutex_unlock(&philo->thread_lock[whoami->left_i]);
+	philo_print_message(MSG_FORK, philo, whoami);
 	pthread_mutex_unlock(&philo->thread_lock[whoami->right_i]);
-	return (EXIT_FAILURE);
+	philo_print_message(MSG_FORK, philo, whoami);
 }
 
-int	philo_routine_go_eat(t_philo *philo, t_whoami *whoami)
+void	philo_routine_go_eat(t_philo *philo, t_whoami *whoami)
 {
-	if (philo_is_alive_check(whoami, philo))
+	whoami->last_eat_ms = philo_get_time(TIME_MSEC, philo);
+	philo_print_message(MSG_EAT, philo, whoami);
+	philo_usleep(philo->info.eat_ms * 1000, philo, whoami);
+	if (philo->info.meals > 0 && philo->info.meals == ++whoami->meals_cnt)
 	{
-		philo_print_message(MSG_EAT, philo, whoami);
-		whoami->last_eat_ms = philo_get_time(TIME_MSEC, philo);
-		philo_usleep(philo->info.eat_ms * 1000, philo, whoami);
-		if (philo->info.meals > 0 && philo->info.meals == ++whoami->meals_cnt)
-		{
-			pthread_mutex_lock(&philo->status_lock);
-			philo->info.indx--;
-			pthread_mutex_unlock(&philo->status_lock);
-		}
+		pthread_mutex_lock(&philo->status_lock);
+		philo->info.indx--;
+		pthread_mutex_unlock(&philo->status_lock);
 	}
-	pthread_mutex_lock(&philo->try_lock);
-	philo->thread_bool[whoami->left_i] = IS_FREE;
-	philo->thread_bool[whoami->right_i] = IS_FREE;
-	pthread_mutex_unlock(&philo->try_lock);
 	pthread_mutex_unlock(&philo->thread_lock[whoami->left_i]);
 	pthread_mutex_unlock(&philo->thread_lock[whoami->right_i]);
-	if (philo_status_check(philo))
-		return (EXIT_SUCCESS);
-	return (EXIT_FAILURE);
 }
 
 int	philo_routine_go_sleep(t_philo *philo, t_whoami *whoami)
 {
-	if (philo_is_alive_check(whoami, philo))
-	{
-		philo_print_message(MSG_SLEEP, philo, whoami);
-		philo_usleep(philo->info.sleep_ms * 1000, philo, whoami);
-	}
-	if (philo_status_check(philo))
-		return (EXIT_SUCCESS);
-	return (EXIT_FAILURE);
+	philo_print_message(MSG_SLEEP, philo, whoami);
+	philo_usleep(philo->info.sleep_ms * 1000, philo, whoami);
 }
 
-/* ------------------- Private Function Implementation --------------------- */
-
-static int	trylock(size_t i, int delay, t_philo *philo, t_whoami *whoami)
-{
-	int	counter;
-
-	counter = 0;
-	while (TRUE)
-	{
-		pthread_mutex_lock(&philo->try_lock);
-		if (IS_FREE == philo->thread_bool[i])
-		{
-			philo->thread_bool[i] = IS_BUSY;
-			pthread_mutex_unlock(&philo->try_lock);
-			pthread_mutex_lock(&philo->thread_lock[i]);
-			return (EXIT_SUCCESS);
-		}
-		pthread_mutex_unlock(&philo->try_lock);
-		if (!philo_is_alive_check(whoami, philo))
-			return (EXIT_FAILURE);
-		if (0 == counter++ % 10)
-			usleep(delay * 2);
-		else
-			usleep(delay);
-		if (delay < 1000)
-			delay *= 2;
-	}
-	return (EXIT_FAILURE);
-}
